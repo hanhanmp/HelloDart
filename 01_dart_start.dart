5aaec488d84a5ffe57b45a32eec43e665986df5f
hanhanmp <2598294105@qq.com>
2022-06-11 14:16:21 +0000

    新增

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
 //变量https://dart.cn/samples#variables
 void variables() {
   /*var的使用
@@ -40,16 +44,295 @@ void flow_control() {
   }
 }
 
-//函数https://dart.cn/samples#functions
+// 函数 （https://dart.cn/samples#functions）
 int fibonacci(int n) {
   if (n == 0 || n == 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-void main(List<String> args) {
+bool is_hit(String line, String target) {
+  return line.contains(target);
+}
+
+void functions() {
+  print('=========函数======');
+  var fib = fibonacci(9);
+  print('fib 9 is $fib');
+
+  var flybyObjects = [
+    'hello',
+    'hanhan',
+    'happy',
+    '平安喜乐',
+    '万事胜意'
+  ];
+  flybyObjects.where((name) => name.contains('you')).forEach(print);
+
+  var hit_lines = flybyObjects.where((x) => is_hit(x, 'o'));
+  for (var one in hit_lines) {
+    print('one is $one');
+  }
+}
+
+// 注释 (https://dart.cn/samples#comments)
+void comments() {
+  print('=========注释======');
+  /// 这是一个文档注释。
+  /// 文档注释用于为库、类以及类的成员添加注释。
+  /// 像 IDE 和 dartdoc 这样的工具可以专门处理文档注释。
+  /* 也可以像这样使用单斜杠和星号的注释方式 */
+  print('comments prints nothing.');
+}
+
+// 导入 （https://dart.cn/samples#imports）
+void imports() {
+   print('=========导入======');
+
+  var mypi = pi;
+  print('pi is $mypi');
+
+  var x = pi / 4;
+  var y = sin(x);
+  print('sin(pi/4) is $y');
+}
+
+// 类 （https://dart.cn/samples#classes）
+class Spacecraft {
+  String name;
+  DateTime? launchDate;
+
+  // Read-only non-final property
+  int? get launchYear => launchDate?.year;
+
+  // Constructor, with syntactic sugar for assignment to members.
+  Spacecraft(this.name, this.launchDate) {
+    // TODO: init.
+  }
+
+  // Named constructor that forwards to the default one.
+  Spacecraft.unlaunched(String name) : this(name, null);
+
+  // Methods.
+  void describe() {
+    print('Spacecraft: $name');
+
+    // Type promotion dos not work on getters.
+    var launchDate = this.launchDate;
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
+}
+
+void classes() {
+   print('=========类======');
+
+  var voyager = Spacecraft('Voyager I', DateTime(1977, 9, 5));
+  var voyager3 = Spacecraft.unlaunched('Voyager III');
+
+  voyager.describe();
+  voyager3.describe();
+}
+
+// 扩展类（继承） （https://dart.cn/samples#inheritance）
+class Orbiter extends Spacecraft {
+  double altitude;
+
+  Orbiter(String name, DateTime launchDate, this.altitude)
+      : super(name, launchDate);
+
+  @override
+  void describe() {
+    super.describe();
+    print('altitude is $altitude km');
+  }
+}
+
+void inheritance() {
+ print('=========继承======');
+
+  var obt = Orbiter('天宫号', DateTime(2021, 4, 29), 389.2);
+  obt.describe();
+}
+
+// Mixins (https://dart.cn/samples#mixins)
+mixin Piloted {
+  int astronauts = 1;
+
+  void describeCrew() {
+    print('Number of astronauts: $astronauts');
+  }
+}
+
+class PilotedCraft extends Spacecraft with Piloted {
+  PilotedCraft(String name, DateTime launchDate) : super(name, launchDate);
+}
+
+void mixins() {
+   print('=========MiXins======');
+
+  var plt = PilotedCraft('神舟1号', DateTime(1999, 11, 20));
+  plt.describe();
+  plt.describeCrew();
+}
+
+// 接口和抽象类 （https://dart.cn/samples#interfaces-and-abstract-classes）
+class MockSpaceship implements Spacecraft {
+  @override
+  DateTime? launchDate;
+
+  @override
+  String name;
+
+  MockSpaceship(this.name, this.launchDate);
+
+  @override
+  void describe() {
+    print('Mock Spacecraft: $name');
+  }
+
+  @override
+  int? get launchYear => launchDate?.year;
+}
+
+void interface_and_abstract_classes() {
+   print('=========接口和抽象类======');
+
+  var mock = MockSpaceship('测试飞行器', null);
+  mock.describe();
+}
+
+// 异步 （https://dart.cn/samples#async）
+Future<void> the_async() async {
+   print('=========异步======');
+
+  const oneSecond = Duration(seconds: 1);
+
+  Future<void> printWithDelay1(String message) async {
+    await Future.delayed(oneSecond);
+    print(message);
+  }
+
+  printWithDelay1('过了1秒钟. 1');
+  print('done 1.');
+
+  Future<void> printWithDelay2(String message) {
+    return Future.delayed(oneSecond).then((_) {
+      print(message);
+    });
+  }
+
+  printWithDelay2('过了1秒钟. 2');
+  print('done 2.');
+
+  Future<void> createDescriptions(Iterable<String> objects) async {
+    for (final object in objects) {
+      try {
+        var file = File('$object.txt');
+        if (await file.exists()) {
+          var modified = await file.lastModified();
+          print(
+              'File for $object already exists. It was modified on $modified.');
+          continue;
+        }
+        await file.create();
+        await file.writeAsString('Start Describing $object in this file.');
+        print('File for $object created.');
+      } on IOException catch (e) {
+        print('Cannot create description for $object: $e');
+      }
+    }
+  }
+
+  var the_objects = ['hanhan', 'mumu', 'fenfen'];
+  createDescriptions(the_objects);
+
+  await Future.delayed(Duration(seconds: 5));
+}
+
+// Stream （https://www.jianshu.com/p/f9af079782af）
+Future<void> the_stream() async {
+  print('=========Stream======');
+
+  const oneSecond = Duration(seconds: 1);
+
+  StreamController<double> ctl = StreamController<double>();
+  Stream stm = ctl.stream;
+
+  stm.listen((event) {
+    print('event from controller is: $event');
+  });
+
+  Future<void> addWithDelay(value) async {
+    await Future.delayed(oneSecond);
+    ctl.add(value);
+  }
+
+  addWithDelay(11.1);
+  addWithDelay(22.2);
+  addWithDelay(33.3);
+
+  await Future.delayed(Duration(seconds: 5));
+}
+
+// 异常 （https://dart.cn/samples#exceptions）
+Future<void> show_descriptions(flybyObjects) async {
+  try {
+    for (final object in flybyObjects) {
+      var description = await File('$object.txt').readAsString();
+      print(description);
+    }
+  } on IOException catch (ex) {
+    print('Could not describe object: $ex');
+  } finally {
+    flybyObjects.clear();
+  }
+}
+
+void exceptions() {
+   print('=========异常======');
+
+  var flybyObjects = ['hanhan', 'mumu', 'fenfen', 'xixi'];
+  show_descriptions(flybyObjects);
+}
+
+Future<void> main(List<String> args) async {
+  // 变量
   variables();
+
+  // 控制流程
   flow_control();
-  var result = fibonacci(20);
-  print('=============函数========');
-  print('result= $result');
+
+  // 函数
+  functions();
+
+  // 注释
+  comments();
+
+  // 导入
+  imports();
+
+  // 类
+  classes();
+
+  // 扩展类（继承）
+  inheritance();
+
+  // Mixins
+  mixins();
+
+  // 接口和抽象类
+  interface_and_abstract_classes();
+
+  // 异步
+  await the_async();
+
+  // Steam
+  await the_stream();
+
+  // 异常
+  exceptions();
 }
